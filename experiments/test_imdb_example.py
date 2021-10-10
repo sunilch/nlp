a5244a13e7216ae7954e88f8f213dc93e3dbfd4d
sunil <sunil_chidara@yahoo.com>
2021-10-10 10:35:32 +0000

    restructuring

---
+++ experiments/test_imdb_example.py
@@ -0,0 +1,6 @@
+
+from fastai.text.all import *
+
+dls = TextDataLoaders.from_folder(untar_data(URLs.IMDB), valid='test')
+learn = text_classifier_learner(dls, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
+learn.fine_tune(1, 1e-2)
---
+++ experiments/test_vision_example.py
@@ -0,0 +1,14 @@
+
+from fastai.vision.all import *
+path = untar_data(URLs.PETS)/'images'
+
+print(path)
+
+def is_cat(x): return x[0].isupper()
+dls = ImageDataLoaders.from_name_func(
+    path, get_image_files(path), valid_pct=0.2, seed=42,
+    label_func=is_cat, item_tfms=Resize(224), bs=5)
+
+
+learn = cnn_learner(dls, resnet34, metrics=error_rate)
+learn.fine_tune(1)
